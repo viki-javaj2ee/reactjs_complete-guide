c44d27e0be590b362fbf833ce7072c2d04591abd
viki-javaj2ee <viki.javaj2ee@gmail.com>
2020-09-09 17:44:13 +0100

    list and keys

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-//import React,{useState} from 'react';
 import React,{Component} from 'react';
 import './App.css';
 import MyPerson from './Person/Person';
@@ -6,24 +5,11 @@ import MyPerson from './Person/Person';
 class App extends Component{
   state = {
     persons: [
-      {name: 'Viki', age: 25},
-      {name: 'Sai', age: 35},
-      {name: 'GAnesh', age: 45}
+      {id: 'id1', name: 'Viki', age: 25},
+      {id: 'id2', name: 'Sai', age: 35},
+      {id: 'id3', name: 'Ganesh', age: 45}
     ],
     showPersons: false
-  }
-
-  switchNameHandler = (newName)=>{
-    //console.log('Was clicked.');
-    //Do not mutate state directly: this.state.persons[0].name = 'Viki Sangani';
-    this.setState({
-      persons: [
-        {name: 'Viki Sangani', age: 30},
-        {name: newName, age: 35},
-        {name: 'Ganesh', age: 45}
-      ]
-    }
-    );
   }
 
 nameChangedHandler = (event)=>{
@@ -42,6 +28,13 @@ togglePersonsHandler = ()=>{
   this.setState({showPersons: !doesShow})
 }
 
+deletePersonHandler = (personIndex) => {
+  //const persons = this.state.persons;
+  const persons = [...this.state.persons];
+  persons.splice(personIndex,1);
+  this.setState({persons: persons})
+}
+
   render(){
 
     const inlineStyle = {
@@ -57,8 +50,12 @@ togglePersonsHandler = ()=>{
     if (this.state.showPersons){
         persons = (
           <div>
-            {this.state.persons.map(person => {
-              return <MyPerson name={person.name} age={person.age} />
+            {this.state.persons.map((person,index) => {
+              return <MyPerson 
+              click={()=>this.deletePersonHandler(index)} 
+              name={person.name} 
+              age={person.age} 
+              key={person.id}/>
             })}
           </div>
         );
@@ -67,9 +64,6 @@ togglePersonsHandler = ()=>{
     return (
       <div className="App">
        <h1>Hi, I'm React App.</h1>
-       
-       {/*<button onClick={this.switchNameHandler.bind(this,'Prashant')}>Switch Name</button>*/}
-       <button style={inlineStyle} onClick={()=>this.switchNameHandler('Prashant')}>Switch Name</button>
        <button style={inlineStyle} onClick={this.togglePersonsHandler}>Toggle Persons</button>
        {persons}
       </div>
@@ -79,50 +73,3 @@ togglePersonsHandler = ()=>{
 }
 
 export default App;
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// const App = props =>  {
-
-//     const [personsState, setPersonsState] = useState({
-//             persons: [
-//               {name: 'Viki', age: 25},
-//               {name: 'Sai', age: 35},
-//               {name: 'GAnesh', age: 45}
-//             ],
-//             otherState: 'Some other value'
-//         });
-
-//   const [newState, setNewState] = useState('New State');        
-
-//   console.log(personsState,newState);
-
-//   const switchNameHandler = ()=>{
-//     //console.log('Was clicked.');
-//     //Do not mutate state directly: this.state.persons[0].name = 'Viki Sangani';
-//     setPersonsState({
-//       persons: [
-//         {name: 'Viki Sangani', age: 30},
-//         {name: 'Sai', age: 35},
-//         {name: 'GAnesh', age: 45}
-//       ]
-//       // ],
-//       // otherState: personsState.otherState
-//     }
-//     )
-//   }
-
-//     return (
-//       <div className="App">
-//        <h1>Hi, I'm React App.</h1>
-//        <button onClick={switchNameHandler}>Switch Name</button>
-//        <MyPerson name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-//        <MyPerson name={personsState.persons[1].name} age={personsState.persons[1].age}/>
-//        <MyPerson name="Ganesh" age="45">My Hobbies: Racing</MyPerson>
-//       </div>
-//     );
-// }
-
-// export default App;
