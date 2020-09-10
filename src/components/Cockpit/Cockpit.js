5fedc5e61ef976d9f016a8a4227d2faa65fdc8cf
viki-javaj2ee <viki.javaj2ee@gmail.com>
2020-09-10 17:22:11 +0100

    using PureComponent

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -13,12 +13,11 @@ const Cockpit = (props) => {
     useEffect(()=>{
         console.log('[Cockpit.js] useEffect');
 
-        const timer = setTimeout(()=>{
+        setTimeout(()=>{
             alert('Saved data to cloud !');
         },1000);
 
         return () => {
-            clearTimeout(timer);
             console.log('[Cockpit.js] cleanup work in useEffect');
         }
     },[]);
